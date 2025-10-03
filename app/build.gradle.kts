8f73013fe55d7bb1fc42a974a962454ad1bf1862
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-10-03 16:06:16 +0100

    add splash screen and change launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+
+    // Splashscreen for managing app launch screens
+    implementation(libs.androidx.core.splashscreen)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
